b8b97586061b7b38d535cc4a76607eb66c085dde
Krishan Nattar <krishan.nattar@gmail.com>
2019-10-09 13:06:19 -0700

    adding all the files....

--- safezoos/src/main/java/com/lambdaschool/zoos/config/AuthorizationServerConfig.java
+++ safezoos/src/main/java/com/lambdaschool/zoos/config/AuthorizationServerConfig.java
@@ -1,4 +1,68 @@
 package com.lambdaschool.zoos.config;
 
-public class AuthorizationServerConfig {
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+
+
+//authorization controls the tokens
+
+//This is general boilerplate code
+//we are creating an authorization server with this.
+@Configuration //this means it's a configuration file
+@EnableAuthorizationServer
+//when we have a client that wants to access our api...
+//we give them the username and password to access our system.
+//in the future we will move these to an .env variable, but for now we hard code below.
+//we call these client_id and client_secret in oauth2
+
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter
+{
+    static final String CLIENT_ID = "lambda-client";
+    static final String CLIENT_SECRET = "lambda-secret";
+    static final String GRANT_TYPE_PASSWORD = "password";
+    static final String AUTHORIZATION_CODE = "authorization_code";
+    static final String REFRESH_TOKEN = "refresh_token";
+    static final String IMPLICIT = "implicit";
+    static final String SCOPE_READ = "read";
+    static final String SCOPE_WRITE = "write";
+    static final String TRUST = "trust";
+    static final int ACCESS_TOKEN_VALIDITY_SECONDS = 1*60*60; //this will be valid for 1 hour
+    static final int REFRESH_TOKEN_VALIDITY_SECONDS = 6*60*60;
+
+
+    @Autowired
+    private TokenStore tokenStore;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private PasswordEncoder encoder;
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer configurer) throws Exception {
+
+        configurer
+                .inMemory()
+                .withClient(CLIENT_ID)
+                .secret(encoder.encode(CLIENT_SECRET))
+                .authorizedGrantTypes(GRANT_TYPE_PASSWORD, AUTHORIZATION_CODE, IMPLICIT )
+                .scopes(SCOPE_READ, SCOPE_WRITE, TRUST)
+                .accessTokenValiditySeconds(ACCESS_TOKEN_VALIDITY_SECONDS)
+                .refreshTokenValiditySeconds(REFRESH_TOKEN_VALIDITY_SECONDS);
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.tokenStore(tokenStore)
+                .authenticationManager(authenticationManager);
+    }
 }
